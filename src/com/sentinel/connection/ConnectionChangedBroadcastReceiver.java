883a64689cb32b61b995c5b9281ee90e1a391fea
dave.aj.russell <dave.aj.russell@c5eba7f1-0d2a-4881-9ce6-8c7f8030d190>
2013-02-03 11:11:09 +0000

    03/02/13
    !!!!!ALWAYS COMMIT CHANGES!!!!!
    
     - Accidental deletion of a number of activities, almost losing 2 days of significant dev. Reverse-engineered from compiled .class files.
     
     ACTUAL UPDATES:
     
      - Ironed out various errors
       - location updates not shutting off
       - improved accuracy of tracking
       - various fixes to handle interrupts (orientation, back button, closing app)
     - Added in alerts if the user turns off GPS
     - Google Maps V2 API now in use
     - Google play services library added as a dependancy

---
+++ src/com/sentinel/connection/ConnectionChangedBroadcastReceiver.java
@@ -0,0 +1,19 @@
+package com.sentinel.connection;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+//import android.net.NetworkInfo;
+
+/**
+ * David Russell
+ * 11/12/12
+ */
+public class ConnectionChangedBroadcastReceiver extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager oConnectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        //NetworkInfo oNetInfo = oConnectivityManager.getActiveNetworkInfo();
+    }
+}
